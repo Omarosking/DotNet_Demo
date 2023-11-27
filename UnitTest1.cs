00c059a77fea4eb65aa3b8fca83e88cd2ad1e3f9
Omarosking <agimar@hotmail.es>
2023-11-27 19:20:58 -0400

    pom2

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -43,4 +43,36 @@ public class Tests
         await formpage.ClickSubmit();
 
     }
+
+     public async Task TestPOM2()
+    {
+        //Playwright
+        using var playwright = await Playwright.CreateAsync();
+       
+        //Browser 
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless  = true
+           
+        });
+       
+        //Page 
+        var page = await browser.NewPageAsync();
+
+    
+        await page.GotoAsync("https://demoqa.com/");
+        await page.SetViewportSizeAsync( width: 1920, height: 1080 );
+
+        Formpage formpage = new Formpage(page);
+        await formpage.ClickForms();
+        await formpage.ClickPracticeForm();
+        await formpage.TextFname();
+        await formpage.TextLname();
+        await formpage.TextEmail();
+        await formpage.CheckGender();
+        await formpage.TextMobile();
+        await formpage.SelectHobbie();
+        await formpage.ClickSubmit();
+
+    }
 } 
